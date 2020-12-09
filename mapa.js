94aeaa630f59cb7519e9c0a1eec7ae18305e6136
Lizeth <elhiguerac@usp.br>
2020-12-08 19:04:15 -0500

    FIx minor details

--- mapa.js
+++ mapa.js
@@ -1,12 +1,18 @@
-//Script hecho por Alejandro Higuera Castro para la clase de Gráfica Interactiva
+//Script hecho por Alejandro Higuera Castro para la clase de Gráfica Interactiva con la libreria p5.js
 
+//Declaración de variables asociadas al script
+
+//Variables para contener las imágenes
 var fondo, tarjeta, tarjeta1, tarjeta2, tarjeta3, tarjeta4, tarjeta5;
 var botonAtras, botonAdelante, botonAdelanteAlpha, botonAtrasAlpha;
+//Variables booleanas de los botones que proveen interactividad
 var boton1 = false, boton2 = false, boton3 = false, boton4 = false, boton5 = false;
+//Variable que determina la posición actual del mapa
 var posX=0;
 var canvas;
 var sizeX;
 
+//La función preload permite cargar los archivos necesarios para la ejecución del Script (Como en Phaser)
 function preload(){
   fondo = loadImage("archivos/fondo.png");
   tarjeta = loadImage("archivos/vacio.png");
@@ -21,6 +27,7 @@ function preload(){
   botonAdelante = loadImage("archivos/adelante.png");
 }
 
+//La función setup permite configurar el canvas antes de su ejecución
 function setup() {
   canvas = createCanvas(360,400);
   // Vincula al elemento con id canvas, el lienzo generado por p5.js
@@ -28,8 +35,9 @@ function setup() {
   fondo.resize(0,800);
 }
 
+//Está función permite redimensionar el canvas de acuerdo al tamaño del dispositivo donde se visualiza
 function adjustCanvas(){
-  print(window.innerWidth);
+  //Se hace una evaluación del ancho del dispositivo
   if(window.innerWidth>860){
     sizeX = 840;
   }else if(window.innerWidth<860 & window.innerWidth>=640){
@@ -37,10 +45,13 @@ function adjustCanvas(){
   }else{
     sizeX = document.body.clientWidth-40;
   }
+  //Se redimensiona el canvas de acuerdo con la evaluación anterior del ancho del dispositivo
   resizeCanvas(sizeX,400);
+  //Se modifica el estilo del canvas generado automáticamente por p5.js con el fin de permitir el RWD
   document.getElementById("defaultCanvas0").style = "width:"+sizeX+"px; height:400px; margin:auto; display:block;";
 }
 
+//Está función contiene las acciones que se repiten durante la ejecución del script (Similar a la función update() de Phaser)
 function draw() {
   adjustCanvas();
   background(220);
@@ -55,18 +66,21 @@ function draw() {
   displayTarjetas();
 }
 
+//Está función permite desplazarse por la imagen panorámica de fondo con ayuda del teclado
 function pasarFondo(){
   if(keyIsPressed){
+    //Si presionamos la tecla flecha derecha el fondo avanzará hacia la derecha
     if(keyCode==39 & posX>=-655){
       posX -=5;
+    //Si presionamos la tecla flecha izquierda el fondo avanzará hacia la izquierda
     }else if(keyCode==37 & posX<0){
       posX +=5;
     }
   }
-  print(posX);
   translate(posX,0);
 }
 
+//Está función permite desplazarse por la imagen panorámica de fondo con ayuda de botones de navegación
 function botonesAvance(){
   if(mouseIsPressed & (20<=mouseX & 40>=mouseX) & (360<=mouseY & 400>=mouseY)){
     image(botonAtras,20-posX,350);
@@ -86,6 +100,7 @@ function botonesAvance(){
   }
 }
 
+//Está función contiene toda la interactividad asociada al botón del barril del Chavo
 function botonBarril(){
   var x = 300;
   var y = 320;
@@ -105,6 +120,7 @@ function botonBarril(){
   pop();
 }
 
+//Está función contiene toda la interactividad asociada al botón del departamento 23
 function botonDepJaimito(){
   var x = 310;
   var y = 200;
@@ -124,6 +140,7 @@ function botonDepJaimito(){
   pop();
 }
 
+//Está función contiene toda la interactividad asociada al botón del departamento 14
 function botonDepFlorinda(){
   var x = 460;
   var y = 280;
@@ -143,6 +160,7 @@ function botonDepFlorinda(){
   pop();
 }
 
+//Está función contiene toda la interactividad asociada al botón del departamento 71
 function botonDepClotilde(){
   var x = 510;
   var y = 280;
@@ -162,6 +180,7 @@ function botonDepClotilde(){
   pop();
 }
 
+//Está función contiene toda la interactividad asociada al botón del departamento 72
 function botonDepRamon(){
   var x = 780;
   var y = 280;
@@ -181,6 +200,7 @@ function botonDepRamon(){
   pop();
 }
 
+//Está función permite mostrar la tarjeta asociada a cada botón que contiene la información que se quiere mostrar
 function displayTarjetas(){
   if(boton1 | boton2 | boton3 | boton4 | boton5 ){
     image(tarjeta,20-posX,20);
